5049e61c36c8ed3c3da2258013ca4b9b6d62f16b
RickYanashita15 <ryanashita15@gmail.com>
2020-07-06 16:06:54 -0700

    Create Welcome Page

---
+++ lib/pages/shelter_sign_up_extended_page.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:phase1/components/floating_text_fields.dart';
+import 'package:phase1/constants.dart';
+
+class ShelterSignUpExtended extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 60.0),
+          height: MediaQuery.of(context).size.height,
+          color: Colors.white,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text("We're so excited to partner with you!", style: headerStyle),
+              SizedBox(height: 60),
+              FloatingTextField(
+                hintText: 'Enter Shelter Name',
+                onChanged: (val) {
+                  //TODO: Update Shelter Name Variable
+                },
+              ),
+              SizedBox(height: 20),
+              FloatingTextField(
+                hintText: 'Enter a Brief Description',
+                onChanged: (val) {
+                  //TODO: Update Shelter Description Variable
+                },
+              ),
+              SizedBox(height: 20),
+              DescriptionBox(
+                hintText: 'Search for Shelter Location',
+                onChanged: (val) {
+                  //TODO: Update Shelter Location Variable, have autocomplete feature to make it easy to find exact shelter location.
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 24),
+          child: Container(
+              height: 50,
+              child: FlatButton(
+                color: colorScheme.secondary,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+                onPressed: () {
+                  //TODO Update Shelter Profile and navigate to the verification page.
+                },
+                child: Container(
+                    child: Text(
+                  'NEXT',
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                )),
+              )),
+        ),
+      ),
+    );
+  }
+}
