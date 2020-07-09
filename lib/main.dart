31d5d75a69e386174223b4b9615e9d68f23757a2
ethayu <ethayu1@gmail.com>
2020-07-09 14:05:02 -0700

    Added firebase iOS functionality

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:phase1/pages/shelter_confirmation_page.dart';
 import 'package:phase1/pages/shelter_sign_up_extended_page.dart';
 
-import 'pages/volunteer_confirmation_page.dart';
-import 'pages/volunteer_sign_up_page.dart';
+import 'constants.dart';
 import 'pages/shelter_sign_up_page.dart';
 import 'pages/signup_page.dart';
+import 'pages/volunteer_confirmation_page.dart';
+import 'pages/volunteer_sign_up_page.dart';
 import 'pages/welcome_page.dart';
-import 'constants.dart';
 
 void main() {
   runApp(MyApp());
@@ -20,18 +20,17 @@ class MyApp extends StatelessWidget {
       title: 'Shelter Connect',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme : colorScheme,
+        colorScheme: colorScheme,
       ),
       routes: {
         '/': (_) => WelcomePage(),
-        '/volunteer_confirmation':(_) => VolunteerConfirmation(),
+        '/volunteer_confirmation': (_) => VolunteerConfirmation(),
         '/volunteer_sign_up': (_) => VolunteerSignUp(),
         '/shelter_confirmation': (_) => ShelterConfirmation(),
         '/shelter_sign_up': (_) => ShelterSignUp(),
         '/shelter_sign_up_extended': (_) => ShelterSignUpExtended(),
         '/signup': (_) => SignupPage(),
       },
-      initialRoute: '/shelter_sign_up_extended',
     );
   }
 }
