ddfefe562d3dac279e35be53cc0e593a2daa08a0
ethayu <ethayu1@gmail.com>
2020-07-09 22:31:06 -0700

    Updated floating text field for obscureText functionality Added backend to organization sign up page

--- lib/components/floating_text_field.dart
+++ lib/components/floating_text_field.dart
@@ -4,8 +4,13 @@ class FloatingTextField extends StatelessWidget {
   final String hintText;
   final Function onChanged;
   final int maxLines;
+  final bool obscureText;
 
-  FloatingTextField({this.hintText, this.onChanged, this.maxLines = 1});
+  FloatingTextField(
+      {this.hintText,
+      this.obscureText = false,
+      this.onChanged,
+      this.maxLines = 1});
 
   @override
   Widget build(BuildContext context) {
@@ -13,6 +18,7 @@ class FloatingTextField extends StatelessWidget {
       elevation: 5,
       borderRadius: BorderRadius.circular(10),
       child: TextField(
+        obscureText: obscureText,
         onChanged: onChanged,
         maxLines: maxLines,
         keyboardType: TextInputType.text,
--- lib/pages/organization_sign_up_page.dart
+++ lib/pages/organization_sign_up_page.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../components/floating_text_field.dart';
@@ -6,6 +8,9 @@ import '../components/text_button.dart';
 import '../constants.dart';
 
 class OrganizationSignUp extends StatelessWidget {
+  final _auth = FirebaseAuth.instance;
+  String email, password, password2;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,29 +27,48 @@ class OrganizationSignUp extends StatelessWidget {
               FloatingTextField(
                 hintText: 'Organization Email',
                 onChanged: (val) {
-                  //TODO: Update E-mail variable
+                  email = val;
                 },
               ),
               SizedBox(height: 20),
               FloatingTextField(
+                obscureText: true,
                 hintText: 'Password',
                 onChanged: (val) {
-                  //TODO: Update Password Variable
+                  password = val;
                 },
               ),
               SizedBox(height: 20),
               FloatingTextField(
+                obscureText: true,
                 hintText: 'Re-enter Password',
                 onChanged: (val) {
-                  //TODO: Update Re-enter Password Variable
+                  password2 = val;
                 },
               ),
               SizedBox(height: 30),
               RoundedButton(
                 title: 'Next',
-                onPressed: () {
-                  Navigator.pushNamed(
-                      context, '/organization_sign_up_extended');
+                onPressed: () async {
+                  if (password != password2) {
+                    showDialog(
+                        context: context,
+                        builder: (BuildContext context) => CupertinoAlertDialog(
+                              title: Text('Passwords do Not Match'),
+                            ));
+                  } else {
+                    try {
+                      final newUser =
+                          await _auth.createUserWithEmailAndPassword(
+                              email: email, password: password);
+                      if (newUser != null) {
+                        Navigator.pushNamed(
+                            context, '/organization_sign_up_extended');
+                      }
+                    } catch (e) {
+                      print(e);
+                    }
+                  }
                 },
               ),
               Padding(
