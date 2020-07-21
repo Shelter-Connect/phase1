cd2de2d8403994c022bfd078eb7fac94cf06a11b
nathangong <nathangong1234@gmail.com>
2020-07-21 14:14:16 -0700

    Update request backend structure

--- lib/services/firestore_helper.dart
+++ lib/services/firestore_helper.dart
@@ -3,16 +3,30 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../constants.dart';
-import '../models/donation.dart';
+import '../models/item.dart';
 import '../models/user.dart';
 
 class FirestoreHelper {
-  CollectionReference getOrganizationDonationReference(BuildContext context) {
-    return db.collection('organizations').document(Provider.of<User>(context).user.uid).collection('donations');
+  static DocumentReference getOrganizationReference(BuildContext context) {
+    return db.collection('organizations').document(Provider.of<User>(context, listen: false).user.uid);
   }
 
-  void addDonation({BuildContext context, Donation donation}) {
-    CollectionReference donationCollectionReference = getOrganizationDonationReference(context);
-    donationCollectionReference.add(donation.toFirestoreMap());
+  static Future<void> createRequest({BuildContext context, List<Item> items}) async {
+    DocumentReference organizationReference = getOrganizationReference(context);
+    CollectionReference requestsReference = organizationReference.collection('requests');
+
+    for (Item item in items) {
+      QuerySnapshot document = await requestsReference.where('name', isEqualTo: item.name).where('type', isEqualTo: item.type).getDocuments();
+      if (document.documents.length == 0) {
+        await requestsReference.add(item.toFirestoreMap());
+      } else if (document.documents.length == 1) {
+        DocumentSnapshot itemSnapshot = document.documents[0];
+        await requestsReference.document(itemSnapshot.documentID).updateData({
+          'amount': item.amount + itemSnapshot['amount'],
+        });
+      } else {
+        throw new Exception('ERROR: Cannot have duplicate items with same name and type in collection');
+      }
+    }
   }
 }
