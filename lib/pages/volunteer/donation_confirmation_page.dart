4b949fec7bc60e5f5f71cfbb545dea886318a2a3
ethayu <ethayu1@gmail.com>
2020-07-24 19:02:52 -0700

    created donation class

--- lib/pages/volunteer/donation_confirmation_page.dart
+++ lib/pages/volunteer/donation_confirmation_page.dart
@@ -18,158 +18,159 @@ class _DonationConfirmationPageState extends State<DonationConfirmationPage> {
   @override
   Widget build(BuildContext context) {
     return StandardLayout(
-        title: ' ',
-        helpText: 'If u don\'t know how to use this app u stupid lmao',
-        body: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 4),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      'Confirm Your Donation for',
-                      style: TextStyle(fontSize: 24, fontWeight: FontWeight.w900, color: purpleAccent),
-                    ),
-                    Text(
-                      widget.organization.name,
-                      style: TextStyle(fontSize: 35, fontWeight: FontWeight.w900, color: purpleAccent),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 4),
-                child: Text(
-                  'Distance: ${widget.organization.distance.toStringAsFixed(1)}',
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.w700, color: purpleAccent),
-                ),
-              ),
-              SizedBox(height: 20),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                child: Container(
-                  decoration: BoxDecoration(color: colorScheme.background, borderRadius: BorderRadius.all(Radius.circular(20))),
-                  width: MediaQuery.of(context).size.width,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 4.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: <Widget>[
-                                Text(
-                                  'Selected Donations',
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                  ),
-                                ),
-                              ],
-                            ),
-                            Container(
-                              height: 5,
-                              width: 100,
-                              decoration: BoxDecoration(color: purpleAccent, borderRadius: BorderRadius.circular(21)),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Text('Blankets x 4', style: TextStyle(fontSize: 17)),
-                            SizedBox(
-                              height: 5,
-                            ),
-                            Text('Bananas x 15', style: TextStyle(fontSize: 17)),
-                            SizedBox(
-                              height: 5,
-                            ),
-                            Text('Can of Beans x 10', style: TextStyle(fontSize: 17)),
-                            SizedBox(
-                              height: 10,
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
-              Column(
+      title: ' ',
+      helpText: 'If u don\'t know how to use this app u stupid lmao',
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 4),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                    child: Container(
-                      decoration: BoxDecoration(color: colorScheme.background, borderRadius: BorderRadius.all(Radius.circular(20))),
-                      width: MediaQuery.of(context).size.width,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16.0),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              'Organization Information ',
-                              style: TextStyle(
-                                fontSize: 20,
-                              ),
-                            ),
-                            SizedBox(
-                              height: 5,
-                            ),
-                            Container(
-                              height: 5,
-                              width: 100,
-                              decoration: BoxDecoration(color: purpleAccent, borderRadius: BorderRadius.circular(21)),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            InfoText(orgEmail: 'loavesandfishes@gmail.com', orgPhone: '408-164-1745', orgAddress: '8164 Fish Pasta Dr.'),
-                          ],
-                        ),
-                      ),
-                    ),
+                  Text(
+                    'Confirm Your Delivery to:',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.w900, color: purpleAccent),
+                  ),
+                  Text(
+                    widget.organization.name,
+                    style: TextStyle(fontSize: 35, fontWeight: FontWeight.w900, color: purpleAccent),
                   ),
                 ],
               ),
-              SizedBox(height: 25),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  child: FlatButton(
-                    onPressed: () {
-                      //TODO Update Firebase and input new order
-                    },
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30.0),
-                    ),
-                    color: purpleAccent,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 4),
+              child: Text(
+                'Distance: ${widget.organization.distance.toStringAsFixed(1)} miles',
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.w700, color: purpleAccent),
+              ),
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Container(
+                decoration: BoxDecoration(color: colorScheme.background, borderRadius: BorderRadius.all(Radius.circular(20))),
+                width: MediaQuery.of(context).size.width,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 4.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Text(
+                                'Selected Donations',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Container(
+                            height: 5,
+                            width: 100,
+                            decoration: BoxDecoration(color: purpleAccent, borderRadius: BorderRadius.circular(21)),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text('Blankets x 4', style: TextStyle(fontSize: 17)),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text('Bananas x 15', style: TextStyle(fontSize: 17)),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text('Can of Beans x 10', style: TextStyle(fontSize: 17)),
+                          SizedBox(
+                            height: 10,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Container(
+                    decoration: BoxDecoration(color: colorScheme.background, borderRadius: BorderRadius.all(Radius.circular(20))),
+                    width: MediaQuery.of(context).size.width,
                     child: Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 20),
-                      child: Text(
-                        'Confirm Your Delivery',
-                        style: TextStyle(color: colorScheme.onSecondary, fontSize: 20),
+                      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'Organization Information ',
+                            style: TextStyle(
+                              fontSize: 20,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Container(
+                            height: 5,
+                            width: 100,
+                            decoration: BoxDecoration(color: purpleAccent, borderRadius: BorderRadius.circular(21)),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          InfoText(orgEmail: 'loavesandfishes@gmail.com', orgPhone: '408-164-1745', orgAddress: '8164 Fish Pasta Dr.'),
+                        ],
                       ),
                     ),
                   ),
                 ),
+              ],
+            ),
+            SizedBox(height: 25),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                child: FlatButton(
+                  onPressed: () {
+                    //TODO Update Firebase and input new order
+                  },
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  color: purpleAccent,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 20),
+                    child: Text(
+                      'Confirm Your Delivery',
+                      style: TextStyle(color: colorScheme.onSecondary, fontSize: 20),
+                    ),
+                  ),
+                ),
               ),
-              SizedBox(height: 10)
-            ],
-          ),
-        ));
+            ),
+            SizedBox(height: 10)
+          ],
+        ),
+      ),
+    );
   }
 }
 
