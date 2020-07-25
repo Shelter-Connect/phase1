f209bd858a26faf8ef4580e0fe725f7bd2a302fe
ethayu <ethayu1@gmail.com>
2020-07-25 14:43:19 -0700

    bug fix

--- lib/pages/volunteer/volunteer_donate_page.dart
+++ lib/pages/volunteer/volunteer_donate_page.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:phase1/components/date_time_field.dart';
-import 'package:phase1/components/volunteer_donate_page_item_selection.dart';
-import 'package:phase1/models/donation.dart';
 import 'package:phase1/components/item_increment.dart';
+import 'package:phase1/models/donation.dart';
 import 'package:phase1/models/item.dart';
 import 'package:phase1/models/organization.dart';
 import 'package:phase1/models/user.dart';
@@ -51,64 +50,75 @@ class _VolunteerDonatePageState extends State<VolunteerDonatePage> {
                 '${widget.organization.distance.toStringAsFixed(1)} miles away',
                 style: TextStyle(fontSize: 25, fontWeight: FontWeight.w700, color: purpleAccent),
               ),
-              BasicDateTimeField(donation: donation,),
+              BasicDateTimeField(
+                donation: donation,
+              ),
               SizedBox(height: 20),
-              ...widget.organization.requestedItems.map((category, categoryItems) => MapEntry(category,
-                Column(
-                  children: [
-                    Container(
-                    decoration: BoxDecoration(color: colorScheme.onSecondary, borderRadius: BorderRadius.all(Radius.circular(20))),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Row(
-                            children: <Widget>[
-                              Text(
-                                category,
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                            ],
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
+              ...widget.organization.requestedItems
+                  .map((category, categoryItems) => MapEntry(
+                      category,
+                      Column(
+                        children: [
                           Container(
-                            height: 5,
-                            width: 50,
-                            decoration: BoxDecoration(color: purpleAccent, borderRadius: BorderRadius.circular(21)),
+                            decoration: BoxDecoration(color: colorScheme.onSecondary, borderRadius: BorderRadius.all(Radius.circular(20))),
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8.0),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  SizedBox(
+                                    height: 10,
+                                  ),
+                                  Row(
+                                    children: <Widget>[
+                                      Text(
+                                        category,
+                                        style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.w500,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    height: 10,
+                                  ),
+                                  Container(
+                                    height: 5,
+                                    width: 50,
+                                    decoration: BoxDecoration(color: purpleAccent, borderRadius: BorderRadius.circular(21)),
+                                  ),
+                                  SizedBox(
+                                    height: 10,
+                                  ),
+                                  ...categoryItems
+                                      .asMap()
+                                      .map((index, item) => MapEntry(
+                                          index,
+                                          Column(
+                                            children: [
+                                              ItemIncrementWithText(
+                                                itemName: item.name,
+                                                maxQuantity: item.amount,
+                                                onChanged: (val) {
+                                                  //TODO
+                                                },
+                                              ),
+                                              SizedBox(height: 10.0),
+                                            ],
+                                          )))
+                                      .values
+                                      .toList(),
+                                ],
+                              ),
+                            ),
                           ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          ...categoryItems.asMap().map((index, item) => MapEntry(index,
-                            Column(
-                              children: [
-                                ItemIncrementWithText(
-                                  itemName: item.name,
-                                  maxQuantity: item.amount,
-                                  onChanged: (val) {
-                                    //TODO
-                                  },
-                                ),
-                                SizedBox(height: 10.0),
-                              ],
-                          ))).values.toList(),
+                          SizedBox(height: 15.0),
                         ],
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 15.0),
-                ],
-              ))).values.toList(),
+                      )))
+                  .values
+                  .toList(),
               SizedBox(height: 15),
               Container(
                 width: MediaQuery.of(context).size.width,
