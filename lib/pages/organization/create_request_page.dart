b2da7c7e9876e56dc4c2aeca87620b321f6acf52
nathanpalamuttam <natepal04@gmail.com>
2020-07-25 23:15:50 -0700

    change increment.dart to be one file

--- lib/pages/organization/create_request_page.dart
+++ lib/pages/organization/create_request_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:phase1/components/floating_text_field.dart';
-import 'package:phase1/components/item_increment.dart';
+import 'package:phase1/components/increment.dart';
 import 'package:phase1/components/secondary_layout.dart';
 import 'package:phase1/constants.dart';
 import 'package:phase1/pages/organization/choose_item_page.dart';
@@ -185,7 +185,7 @@ class _CreateRequestPageState extends State<CreateRequestPage> {
                   //TODO Save Information to Firebase
                 ),
                 SizedBox(height: 20),
-                ItemIncrementWithoutText(),
+                ItemIncrementWithText(),
                 SizedBox(height: 20),
               ],
             ),
