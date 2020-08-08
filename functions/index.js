e8aac4166806f712deb99411f3a8758852fca4f4
nathangong <nathangong1234@gmail.com>
2020-08-08 16:11:09 -0700

    Create update organization info firebase function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,20 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+const db = admin.firestore();
+
+exports.updateOrganizationInfo = functions.firestore.document('organizations/{uid}').onUpdate(async (doc, context) => {
+    let updatedInfo = doc.after;
+    let currentDonations = await db.collection('organizations').doc(context.params.uid).collection('currentDonations').get();
+    for (const donation of currentDonations.docs) {
+        let volunteerId = donation.data().volunteerId;
+        let volunteerDonationReference = db.collection('volunteers').doc(volunteerId).collection('currentDonations').doc(donation.id);
+        volunteerDonationReference.update({
+            organizationAddress: updatedInfo.data().address,
+            organizationDescription: updatedInfo.data().description,
+            organizationLocation: updatedInfo.data().location,
+            organizationName: updatedInfo.data().name,
+        });
+    }
+});
