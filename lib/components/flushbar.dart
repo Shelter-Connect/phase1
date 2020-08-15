cb00529cd26a7fe4365e4187522b96619ccfd5b8
nathanpalamuttam <natepal04@gmail.com>
2020-08-15 12:21:05 -0700

    Push error with component of flushbar.dart + icon change

---
+++ lib/components/flushbar.dart
@@ -0,0 +1,28 @@
+import 'package:flushbar/flushbar.dart';
+import 'package:flutter/material.dart';
+import 'package:phase1/constants.dart';
+
+class FlushBar extends StatelessWidget {
+  final String title, message;
+  final Icon icon;
+  final Duration duration;
+  final Widget mainButton;
+  final Function onTap;
+  final bool isDismissible;
+  FlushBar({this.title, this.message, this.icon, this.duration, this.mainButton, this.onTap, this.isDismissible});
+  @override
+  Widget build(BuildContext context) {
+    return Flushbar(
+      title: title ?? null,
+      message: message ?? null,
+      icon: icon ?? Icon(Icons.info_outline),
+      leftBarIndicatorColor: purpleAccent,
+      duration: duration ?? Duration(seconds: 3),
+      mainButton: mainButton,
+      onTap: onTap,
+      isDismissible: isDismissible ?? true,
+    ).show(context);
+
+  }
+}
+
